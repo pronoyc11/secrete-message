187ccf1e9a370cde1dd8bcbb7876b64866dd94eb
pronoyc11 <pronoyc11@gmail.com>
2024-04-05 19:51:31 +0600

    bg-correction12345

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const submitButton = document.querySelector("#submit-button");
 themeButton.addEventListener("click", changeTheme);
 //THME OPERATION ENDS
 //CODE form operation starts
-codeForm.addEventListener("submit",decryptCode);
+codeForm.addEventListener("submit", decryptCode);
 //codeValue.addEventListener("")
 //CODE form operation ends
 //SHOS MESSAGE operation starts
@@ -23,148 +23,141 @@ codeForm.addEventListener("submit",decryptCode);
 //THEME FUNCTION STARTS
 
 function changeTheme() {
-    const themeArr = [
-      'url("https://pronoyc11.github.io/secrete-message/images/BluePaper.jpg")',
-      'url("https://pronoyc11.github.io/secrete-message/images/rose-bg.jpg")',
-      'url("https://pronoyc11.github.io/secrete-message/images/TelephoneBg.jpg")',
-    ];
+
+  const themeArr = [
+    "https://img.freepik.com/free-photo/blue-paper-red-flower-petals_23-2148031526.jpg?size=626&ext=jpg&uid=R143827644&ga=GA1.1.407465969.1712125518&semt=ais",
+    "https://img.freepik.com/free-photo/red-rose-with-white-card_1150-6925.jpg?w=900",
+    "https://growgreencapital.co.in/wp-content/uploads/2023/05/con-12.jpeg",
+  ];
+  // const themeArr = [
+  //   'url("https://pronoyc11.github.io/secrete-message/images/BluePaper.jpg")',
+  //   'url("https://pronoyc11.github.io/secrete-message/images/rose-bg.jpg")',
+  //   'url("https://pronoyc11.github.io/secrete-message/images/TelephoneBg.jpg")',
+  // ];
   let style = getComputedStyle(body);
   // let codeFormStyle = getComputedStyle(codeForm)
   // let colorValue = codeFormStyle.backgroundColor ;
   // console.log(colorValue); url("images/BluePaper.jpg")
-  let urlValue = style.getPropertyValue("background-image");
-    
-  let themeIndex = themeArr.indexOf(urlValue);
-  console.log("url:",urlValue)
-  console.log("index:",themeIndex)
-  if (themeIndex === themeArr.length - 1) {
-      body.style.backgroundImage = `url("${themeArr[0].slice(49)}`;
-    } else {
-        let str = `url("${themeArr[themeIndex + 1].slice(49)}`;
-        body.style.backgroundImage = str;
-        console.log(str);
-    }
-    if (urlValue === themeArr[1]) {
-      codeForm.style.backgroundColor = "#C6DBC6";
-      
-    } else {
-      codeForm.style.backgroundColor = "";
-    }
+  let urlValue = style.getPropertyValue("background-image").slice(5,-2);
 
+  let themeIndex = themeArr.indexOf(urlValue);
+
+  if (themeIndex === themeArr.length - 1) {
+    body.style.backgroundImage = `url("${themeArr[0]}")`;
+  } else {
+    let str = `url("${themeArr[themeIndex + 1]}")`;
+    body.style.backgroundImage = str;
+  }
+  if (urlValue === themeArr[1]) {
+    codeForm.style.backgroundColor = "#84C3CA";
+  } else {
+    codeForm.style.backgroundColor = "";
+  }
+ 
 }
 //THEME FUNCTION ENDS
 //CODE Decryption function starts
-function encryptMsg(str){
-
-    let popedArr = str.trim().split(' ') ;
-    let num = popedArr.pop();
-    let strArr = popedArr.map((elm,index)=>{
-      elm = elm.split("");
-      for(let i = 0 ; i < elm.length ; i ++){
-    
-        if((i+1)%2===0){
-          switch (elm[i]) {
-            case "a":
-              elm[i] = "y";
-              break;
-            case "A":
-              elm[i] = "Y";
-              break;
-            case "b":
-              elm[i] = "z"
-              break;
-            case "B":
-              elm[i] = "Z";
-              break;
-            case " ":
-              break;
-            case ",":
-              elm[i] = "," ;
-              break;
-            case "!":
-              elm[i] = "!";
-              break;
-            case "?":
-              elm[i] = "?";
-              break;
-            default:
-              elm[i] = String.fromCharCode(elm[i].charCodeAt(0)-2);
-          }
-          
-        }else if((i+1)%2!== 0){
-    if(elm[i]=== "z"){
-      elm[i] = "b";
-    }else if(elm[i] === "Z"){
-      elm[i] = "B"
-    }else if(elm[i]=== " "){
-    
-    }else if(elm[i]=== "y"){
-    elm[i] = 'a' ;
-    }else if(elm[i] === "Y"){
-       elm[i] = "A";
-    }else if(elm[i] === ","){
-        elm[i] = ","
-    }else if(elm[i] === "?"){
-      elm[i] = "?"
-    }else if(elm[i] === "!"){
-      elm[i] = "!"
-    }
-    else{
-      elm[i] = String.fromCharCode(elm[i].charCodeAt(0)+2);
-    }
-    
-          
+function encryptMsg(str) {
+  let popedArr = str.trim().split(" ");
+  let num = popedArr.pop();
+  let strArr = popedArr.map((elm, index) => {
+    elm = elm.split("");
+    for (let i = 0; i < elm.length; i++) {
+      if ((i + 1) % 2 === 0) {
+        switch (elm[i]) {
+          case "a":
+            elm[i] = "y";
+            break;
+          case "A":
+            elm[i] = "Y";
+            break;
+          case "b":
+            elm[i] = "z";
+            break;
+          case "B":
+            elm[i] = "Z";
+            break;
+          case " ":
+            break;
+          case ",":
+            elm[i] = ",";
+            break;
+          case "!":
+            elm[i] = "!";
+            break;
+          case "?":
+            elm[i] = "?";
+            break;
+          default:
+            elm[i] = String.fromCharCode(elm[i].charCodeAt(0) - 2);
         }
-     
+      } else if ((i + 1) % 2 !== 0) {
+        if (elm[i] === "z") {
+          elm[i] = "b";
+        } else if (elm[i] === "Z") {
+          elm[i] = "B";
+        } else if (elm[i] === " ") {
+        } else if (elm[i] === "y") {
+          elm[i] = "a";
+        } else if (elm[i] === "Y") {
+          elm[i] = "A";
+        } else if (elm[i] === ",") {
+          elm[i] = ",";
+        } else if (elm[i] === "?") {
+          elm[i] = "?";
+        } else if (elm[i] === "!") {
+          elm[i] = "!";
+        } else {
+          elm[i] = String.fromCharCode(elm[i].charCodeAt(0) + 2);
+        }
       }
-      elm = elm.join("");
-      return elm;
-    })
-    
-    if(num.length === 4){
-      if(Number(num)){
-           if(parseInt(num[0]) + parseInt(num[1]) === 10 && parseInt(num[2]) + parseInt(num[3]) === 10){
-            return strArr.join(" ") ;
-           }else{
-            return "Invalid numeric code!!";
-           }
-      }else{
-         return "Numeric value invalid!";
+    }
+    elm = elm.join("");
+    return elm;
+  });
+
+  if (num.length === 4) {
+    if (Number(num)) {
+      if (
+        parseInt(num[0]) + parseInt(num[1]) === 10 &&
+        parseInt(num[2]) + parseInt(num[3]) === 10
+      ) {
+        return strArr.join(" ");
+      } else {
+        return "Invalid numeric code!!";
       }
-    }else{
-      return "Please put the code appropriately.";
+    } else {
+      return "Numeric value invalid!";
     }
+  } else {
+    return "Please put the code appropriately.";
+  }
+}
+
+//console.log(encryptMsg("g jqtg wqs! 9182"))
+//console.log(encryptMsg("g jqtg wqs! ukjn wqs kcptw kg? 9182"));
+
+function decryptCode(e) {
+  e.preventDefault();
+  submitButton.setAttribute("disabled", null);
+  let text = showMessage.lastElementChild;
+  userName.innerHTML = `Dear ${nameValue.value.trim().toLowerCase()},`;
+  text.innerHTML = "";
+  let message = encryptMsg(codeValue.value).split("");
+
+  let i = 0;
+
+  let intervalId = setInterval(() => {
+    if (i < message.length) {
+      text.innerHTML += message[i] === " " ? " " : message[i];
+      i++;
+    } else {
+      clearInterval(intervalId);
+      submitButton.removeAttribute("disabled");
     }
-    
-    //console.log(encryptMsg("g jqtg wqs! 9182"))
-    //console.log(encryptMsg("g jqtg wqs! ukjn wqs kcptw kg? 9182"));
+    console.log();
+  }, 100);
 
-
-
-
-
-function decryptCode(e){
-    e.preventDefault();
-    submitButton.setAttribute("disabled",null);
-     let text = showMessage.lastElementChild;
-     userName.innerHTML = `Dear ${nameValue.value.trim().toLowerCase()},`
-     text.innerHTML = "";
-     let message = encryptMsg(codeValue.value).split("");
-
-     let i = 0 ;
-    
-       let intervalId =  setInterval(()=>{
-            if(i<message.length){
-           text.innerHTML +=  message[i] === " "? " ":message[i] ;
-           i++ ;}
-           else {
-            clearInterval(intervalId);
-          submitButton.removeAttribute("disabled");
-           }
-           console.log()
-        },100)
-    
-    //  text.innerHTML = codeValue.value;
-    
+  //  text.innerHTML = codeValue.value;
 }
 //CODE Decryption function ends
